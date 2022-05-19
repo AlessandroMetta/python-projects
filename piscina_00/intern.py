7cd6022e26c4bb70dc565590807380517297e44b
AlessandroMetta <ametta@student.42roma.it>
2022-05-19 15:42:26 +0200

    c

---
+++ piscina_00/intern.py
@@ -0,0 +1,25 @@
+class Intern():
+    def __init__(self, str="My name? I’m nobody, an intern, I have no name."):
+        self.Name = str
+    def __str__(self):
+        return(self.Name)
+    class Coffee():
+        def __str__(self):
+            return("This is the worst coffee you ever tasted.")
+    def work(self):
+        raise Exception("I,’m just an intern, I can’t do that...")
+    def make_coffee(self):
+        return self.Coffee()
+
+
+mark = Intern("Mark")
+none = Intern()
+
+print(mark)
+print(none)
+
+print(mark.make_coffee())
+try:
+    none.work()
+except Exception as e:
+    print(e)
