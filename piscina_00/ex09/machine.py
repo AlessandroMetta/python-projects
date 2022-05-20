64b99e125fb7bf6c63f18328294c05de7b0c3aec
AlessandroMetta <ametta@student.42roma.it>
2022-05-20 10:26:50 +0200

    finished

--- piscina_00/ex09/machine.py
+++ piscina_00/ex09/machine.py
@@ -2,19 +2,26 @@ from beverages import HotBeverage, Coffee, Tea, Chocolate, Cappuccino
 from typing import Type
 import random
 
+
+
+
 class CoffeeMachine():
     def __init__(self):
         self.usage = 0
+
     class EmptyCup(HotBeverage):
         def __init__(self):
             HotBeverage.__init__(self, 0.90, "empty cup")
         def description(self):
             return HotBeverage.description(self, "An empty cup?! Gimme my money back!")
+
     class BrokenMachineException(Exception):
         def __init__(self):
             Exception.__init__(self, 'This coffee machine has to be repaired.')
+
     def repair(self):
         self.usage = 0
+
     def serve(self, beverage: Type[HotBeverage]):
         if self.usage != 10:
             self.usage += 1
@@ -25,45 +32,57 @@ class CoffeeMachine():
         else:
             raise self.BrokenMachineException()
 
+
+
+
+
+
 def __main__():
-    a = CoffeeMachine()
-    print(CoffeeMachine.serve(a, Coffee()).description())
-    print(CoffeeMachine.serve(a, Cappuccino()).description())
-    print(CoffeeMachine.serve(a, Chocolate()).description())
-    print(CoffeeMachine.serve(a, Tea()).description())
-    print(CoffeeMachine.serve(a, Cappuccino()).description())
-    print(CoffeeMachine.serve(a, Coffee()).description())
-    print(CoffeeMachine.serve(a, Chocolate()).description())
-    print(CoffeeMachine.serve(a, Tea()).description())
-    print(CoffeeMachine.serve(a, HotBeverage()).description())
-    print(CoffeeMachine.serve(a, HotBeverage()).description())
+    machine = CoffeeMachine()
+
+    print(CoffeeMachine.serve(machine, Coffee()).description())
+    print(CoffeeMachine.serve(machine, Cappuccino()).description())
+    print(CoffeeMachine.serve(machine, Chocolate()).description())
+    print(CoffeeMachine.serve(machine, Tea()).description())
+    print(CoffeeMachine.serve(machine, Cappuccino()).description())
+    print(CoffeeMachine.serve(machine, Coffee()).description())
+    print(CoffeeMachine.serve(machine, Chocolate()).description())
+    print(CoffeeMachine.serve(machine, Tea()).description())
+    print(CoffeeMachine.serve(machine, HotBeverage()).description())
+    print(CoffeeMachine.serve(machine, HotBeverage()).description())
+
     try:
-    	print(CoffeeMachine.serve(a, Coffee()).description())
+    	print(CoffeeMachine.serve(machine, Coffee()).description())
     except Exception as e:
     	print(e)
     try:
-    	print(CoffeeMachine.serve(a, Coffee()).description())
+    	print(CoffeeMachine.serve(machine, Coffee()).description())
     except Exception as e:
     	print(e)
-    CoffeeMachine.repair(a)
-    print(CoffeeMachine.serve(a, Coffee()).description())
-    print(CoffeeMachine.serve(a, Cappuccino()).description())
-    print(CoffeeMachine.serve(a, Chocolate()).description())
-    print(CoffeeMachine.serve(a, Tea()).description())
-    print(CoffeeMachine.serve(a, Cappuccino()).description())
-    print(CoffeeMachine.serve(a, Coffee()).description())
-    print(CoffeeMachine.serve(a, Chocolate()).description())
-    print(CoffeeMachine.serve(a, Tea()).description())
-    print(CoffeeMachine.serve(a, HotBeverage()).description())
-    print(CoffeeMachine.serve(a, HotBeverage()).description())
+
+    CoffeeMachine.repair(machine)
+
+    print(CoffeeMachine.serve(machine, Coffee()).description())
+    print(CoffeeMachine.serve(machine, Cappuccino()).description())
+    print(CoffeeMachine.serve(machine, Chocolate()).description())
+    print(CoffeeMachine.serve(machine, Tea()).description())
+    print(CoffeeMachine.serve(machine, Cappuccino()).description())
+    print(CoffeeMachine.serve(machine, Coffee()).description())
+    print(CoffeeMachine.serve(machine, Chocolate()).description())
+    print(CoffeeMachine.serve(machine, Tea()).description())
+    print(CoffeeMachine.serve(machine, HotBeverage()).description())
+    print(CoffeeMachine.serve(machine, HotBeverage()).description())
+    
     try:
-    	print(CoffeeMachine.serve(a, Coffee()).description())
+    	print(CoffeeMachine.serve(machine, Coffee()).description())
     except Exception as e:
     	print(e)
     try:
-    	print(CoffeeMachine.serve(a, Coffee()).description())
+    	print(CoffeeMachine.serve(machine, Coffee()).description())
     except Exception as e:
     	print(e)
+
+
 
 if __name__ == '__main__':
     __main__()
